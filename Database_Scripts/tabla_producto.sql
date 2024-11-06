90505124f6463621d07f643b5c8402b2a4365590
Lordbear117 <blackyi_14@hotmail.com>
2024-11-06 06:36:52 -0700

    feat: agregar tablas script

--- Database_Scripts/tabla_producto.sql
+++ Database_Scripts/tabla_producto.sql
@@ -6,3 +6,31 @@ CREATE TABLE `sys`.`producto` (
   `precio` DECIMAL NULL,
   `imagen` VARCHAR(200) NULL,
   PRIMARY KEY (`id_producto`));
+
+  CREATE TABLE sys.users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    role VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+-- Tabla de roles
+CREATE TABLE sys.roles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(20) UNIQUE NOT NULL
+);
+
+-- Tabla de relación entre usuarios y roles
+CREATE TABLE sys.user_roles (
+    user_id INT NOT NULL,
+    role_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
+    PRIMARY KEY (user_id, role_id)
+);
+
+INSERT INTO sys.roles (name) VALUES ('ROLE_USER');
+INSERT INTO sys.roles (name) VALUES ('ROLE_ADMIN');
